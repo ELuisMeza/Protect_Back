3c58e416062a83d451dca9c92a02ac135012d02d
Luis Meza <ednilsonluis.11@gmail.com>
2026-08-26 15:34:11 -0500

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure. Added .gitignore and .gitattributes for environment setup. Configured PostgreSQL datasource in application properties.

---
+++ src/main/java/com/emz/protec/ProtecApplication.java
@@ -0,0 +1,13 @@
+package com.emz.protec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emz/protec/ProtecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emz.protec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
